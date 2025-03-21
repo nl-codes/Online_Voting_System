c857790afc88d4d5f7c49fb96ca3a8b20f0b5126
narayanLohani <narayanlohani07@gmail.com>
2025-03-21 10:08:23 +0545

    Remove Election/Candidate switch from UserDashboard

--- frontend/src/pages/UserDashboardPage.jsx
+++ frontend/src/pages/UserDashboardPage.jsx
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
-import ElectionCandidateSwitchButton from "../components/ElectionCandidateSwitchButton";
 import ProfileDisplay from "../components/ProfileDisplay.jsx";
 
 const UserDashboardPage = () => {
@@ -9,13 +8,12 @@ const UserDashboardPage = () => {
         navigate("/");
     };
 
-    const [selectedMode, setSelectedMode] = useState("election");
     return (
         <div className="bg-[#29142e] h-screen w-screen">
             {/* Header */}
-            <div className="text-white text-2xl flex items-center justify-between px-20">
+            <div className="text-white text-2xl flex items-center justify-between px-20 pt-4">
                 <p>ONLINE VOTING SYSTEM</p>
-                <div>
+                <div className="flex items-center">
                     <ProfileDisplay />
                     <button
                         className="text-xl text-white font-bold hover:text-[#29142e] w-30 h-10 hover:bg-white px-4 my-4 rounded-2xl"
@@ -24,11 +22,7 @@ const UserDashboardPage = () => {
                     </button>
                 </div>
             </div>
-            {/* Election / Candidates */}
-            <ElectionCandidateSwitchButton
-                selectedMode={selectedMode}
-                setSelectedMode={setSelectedMode}
-            />
+            {/* Election Lists */}
         </div>
     );
 };
