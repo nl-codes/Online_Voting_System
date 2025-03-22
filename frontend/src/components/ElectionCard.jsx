c90199a673c3d18accd8960580074bff1389e417
narayanLohani <narayanlohani07@gmail.com>
2025-03-22 06:17:27 +0545

    add ElectionCard

---
+++ frontend/src/components/ElectionCard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ElectionCard = () => {
+    return <div>ElectionCard</div>;
+};
+
+export default ElectionCard;
--- frontend/src/pages/UserDashboardPage.jsx
+++ frontend/src/pages/UserDashboardPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import ProfileDisplay from "../components/ProfileDisplay.jsx";
+import ElectionCard from "../components/ElectionCard.jsx";
 
 const UserDashboardPage = () => {
     const navigate = useNavigate();
@@ -12,7 +13,7 @@ const UserDashboardPage = () => {
         <div className="bg-[#29142e] h-screen w-screen">
             {/* Header */}
             <div className="text-white text-2xl flex items-center justify-between px-20 pt-4">
-                <p>ONLINE VOTING SYSTEM</p>
+                <p className="text-3xl font-bold">ONLINE VOTING SYSTEM</p>
                 <div className="flex items-center">
                     <ProfileDisplay />
                     <button
@@ -22,7 +23,17 @@ const UserDashboardPage = () => {
                     </button>
                 </div>
             </div>
-            {/* Election Lists */}
+            {/* Election Lists Box*/}
+            <div className="text-white text-2xl flex items-center justify-center px-20 pt-4">
+                <p>Available Elections</p>
+                <div className="elections-list">
+                    <ElectionCard
+                        topic="Next PM for Group"
+                        stop_time="2025-03-22"
+                        cadidates_list="[]"
+                    />
+                </div>
+            </div>
         </div>
     );
 };
