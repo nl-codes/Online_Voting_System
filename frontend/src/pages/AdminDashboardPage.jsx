c40fc215e6de3ef1feb9d34d6a4aa2a30ce518ce
narayanLohani <narayanlohani07@gmail.com>
2025-04-08 18:25:34 +0545

    Add Admin Sidebar on Admin Dashboard

--- frontend/src/pages/AdminDashboardPage.jsx
+++ frontend/src/pages/AdminDashboardPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import AdminSidebar from "../components/AdminSidebar";
 
 const AdminDashboardPage = () => {
     const navigate = useNavigate();
@@ -8,13 +9,16 @@ const AdminDashboardPage = () => {
         navigate("/sec/admin/login");
     };
     return (
-        <div className="bg-[#29142e] h-screen w-screen flex flex-col">
-            <div className="flex justify-end p-4">
-                <button
-                    className="hover:bg-[#7d4788] text-white font-bold py-2 px-4 rounded"
-                    onClick={handleLogOut}>
-                    Log out
-                </button>
+        <div className="bg-[#29142e] h-screen w-screen flex pr-8">
+            <AdminSidebar />
+            <div className="flex flex-col p-4 w-full">
+                <div className="flex justify-end">
+                    <button
+                        className="hover:bg-[#7d4788] text-white text-2xl font-bold py-2 px-4 rounded"
+                        onClick={handleLogOut}>
+                        Log out
+                    </button>
+                </div>
             </div>
             <div className="flex flex-col justify-center items-center"></div>
         </div>
