a8e55b61230febcff74cc5dcc72d306b0b0df580
narayanLohani <narayanlohani07@gmail.com>
2025-04-09 11:40:53 +0545

    Show user documents in preview

--- frontend/src/components/VerifyUserCard.jsx
+++ frontend/src/components/VerifyUserCard.jsx
@@ -1,7 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 
-const VerifyUserCard = () => {
-    return <div>VerifyUserCard</div>;
+const VerifyUserCard = ({ userData }) => {
+    const [selectedImage, setSelectedImage] = useState(null);
+
+    // Handle image click
+    const handleImageClick = (imageSrc) => {
+        setSelectedImage(imageSrc);
+    };
+
+    // Handle closing the modal
+    const handleCloseImage = () => {
+        setSelectedImage(null);
+    };
+
+    return (
+        <>
+            <div className="border-4 border-[#7d4788] rounded-2xl px-4 py-2">
+                <div className="user-details text-lg">
+                    <div className="flex gap-20">
+                        UserName :{" "}
+                        <span className="font-bold">{userData.full_name}</span>
+                    </div>
+                    <div className="flex gap-32">
+                        DOB : <span className="font-bold">{userData.dob}</span>
+                    </div>
+                    <div className="flex gap-2">
+                        Citizenship-number :{" "}
+                        <span className="font-bold">
+                            {userData.citizenship_number}
+                        </span>
+                    </div>
+                </div>
+                <div className="photos flex gap-10 my-4">
+                    <img
+                        className="border-white border-2 rounded-lg"
+                        src={userData.citizenship_front_pic}
+                        alt="citizenship_front_pic"
+                        onClick={() =>
+                            handleImageClick(userData.citizenship_front_pic)
+                        }
+                        width={"200px"}
+                    />
+                    <img
+                        className="border-white border-2 rounded-lg"
+                        src={userData.citizenship_back_pic}
+                        alt="citizenship_back_pic"
+                        onClick={() =>
+                            handleImageClick(userData.citizenship_back_pic)
+                        }
+                        width={"200px"}
+                    />
+                </div>
+                <span className="text-[12px]">Click to zoom</span>
+            </div>
+            {/* Modal for full-screen image */}
+            {selectedImage && (
+                <div
+                    className="fixed inset-0 bg-[#7d4788] flex items-center justify-center z-50r"
+                    onClick={handleCloseImage}>
+                    <div className="relative max-w-[90vw] max-h-[90vh]">
+                        <img
+                            src={selectedImage}
+                            alt="Full screen view"
+                            className="max-w-full max-h-[90vh] object-contain"
+                        />
+                        <button
+                            className="absolute left-1/2 -translate-x-1/2 -bottom-10 text-white text-xl bg-red-600 bg-opacity-50 px-4 h-8 rounded-md hover:bg-red-800 cursor-pointer"
+                            onClick={handleCloseImage}>
+                            Close
+                        </button>
+                    </div>
+                </div>
+            )}
+        </>
+    );
 };
 
 export default VerifyUserCard;
--- frontend/src/pages/AdminVerifyVoterPage.jsx
+++ frontend/src/pages/AdminVerifyVoterPage.jsx
@@ -3,6 +3,16 @@ import AdminSidebar from "../components/AdminSidebar";
 import VerifyUserCard from "../components/VerifyUserCard";
 
 const AdminVerifyVoterPage = () => {
+    const dummyUserData = {
+        full_name: "Kurosaki Ichigo",
+        dob: "2005-11-25",
+        citizenship_number: "123456789",
+        phone_number: "9845612347",
+        citizenship_front_pic:
+            "https://res.cloudinary.com/duhbs7hqv/image/upload/v1743821397/citizenship_images/gzxjm3kh5at4f93iffzu.png",
+        citizenship_back_pic:
+            "https://res.cloudinary.com/duhbs7hqv/image/upload/v1743821397/citizenship_images/gzxjm3kh5at4f93iffzu.png",
+    };
     return (
         <div className="bg-[#29142e] h-screen w-screen flex pr-8">
             <AdminSidebar />
@@ -12,10 +22,11 @@ const AdminVerifyVoterPage = () => {
                         Log out
                     </button>
                 </div>
-                <div className="flex flex-col justify-center items-center text-white main-page-content">
-                    <p className="">Verify User Details</p>
+                <div className="flex flex-col justify-center  text-white main-page-content">
+                    <p className="text-center">Verify User Details</p>
                     <div>
-                        <VerifyUserCard />
+                        <p>Remaining User Verifications : {1}</p>
+                        <VerifyUserCard userData={dummyUserData} />
                     </div>
                 </div>
             </div>
