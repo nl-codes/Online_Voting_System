5bd96259f00bdbd71bf84f9e060f1fa3d771b23f
narayanLohani <narayanlohani07@gmail.com>
2025-05-03 15:20:16 +0545

    Added User Profile Page to include User Profile Edit Form

--- frontend/src/pages/UserProfileEditPage.jsx
+++ frontend/src/pages/UserProfileEditPage.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
+import BackButton from "../components/BackButton";
+import { UserContext } from "../context/UserContext";
+import UserProfileForm from "../components/UserProfileForm";
 
 const UserProfileEditPage = () => {
-    return <div>UserProfileEditPage</div>;
+    const { userId } = useContext(UserContext);
+    return (
+        <div className="bg-[#29142e] h-screen max-w-screen py-4 px-4">
+            <div className="flex justify-between items-center ">
+                <BackButton backPath={"/profile"} />
+            </div>
+            <h1 className="w-full text-4xl text-white font-bold text-center pt-10">
+                Edit Your Profile
+            </h1>
+            <UserProfileForm userId={userId} />
+        </div>
+    );
 };
 
 export default UserProfileEditPage;
