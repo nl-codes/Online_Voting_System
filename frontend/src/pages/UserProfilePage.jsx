e2f1d7dcd36252a88ca3f96e4634ea99fc4e5590
narayanLohani <narayanlohani07@gmail.com>
2025-05-03 13:04:04 +0545

    add title to profilepage

--- frontend/src/pages/UserProfilePage.jsx
+++ frontend/src/pages/UserProfilePage.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const UserProfilePage = () => {
-    return <div>UserProfilePage</div>;
+    return (
+        <div className="bg-[#29142e] h-screen max-w-screen">
+            <h1 className="w-full text-4xl text-white font-bold text-center pt-10">
+                User Profile
+            </h1>
+        </div>
+    );
 };
 
 export default UserProfilePage;
