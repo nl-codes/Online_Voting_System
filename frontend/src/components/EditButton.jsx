f4220dca7f43f383f8897df4a92da300fc7b7e3b
narayanLohani <narayanlohani07@gmail.com>
2025-05-03 22:20:16 +0545

    edit clip-rule and fill-rule to clipRule and fillRule

--- frontend/src/components/EditButton.jsx
+++ frontend/src/components/EditButton.jsx
@@ -11,7 +11,7 @@ const EditButton = ({ editPath }) => {
             className="flex items-center w-fit bg-[#29142e] text-white px-4 py-2 rounded-md font-bold cursor-pointer border-2 border-white hover:bg-white hover:text-[#29142e] transition-all duration-150 ease-in-out hover:scale-110 group gap-2"
             onClick={handleEdit}>
             <svg
-                class="w-6 h-6 text-white group-hover:text-[#29142e] transition-colors duration-150"
+                className="w-6 h-6 text-white group-hover:text-[#29142e] transition-colors duration-150"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
@@ -19,14 +19,14 @@ const EditButton = ({ editPath }) => {
                 fill="currentColor"
                 viewBox="0 0 24 24">
                 <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     d="M11.32 6.176H5c-1.105 0-2 .949-2 2.118v10.588C3 20.052 3.895 21 5 21h11c1.105 0 2-.948 2-2.118v-7.75l-3.914 4.144A2.46 2.46 0 0 1 12.81 16l-2.681.568c-1.75.37-3.292-1.263-2.942-3.115l.536-2.839c.097-.512.335-.983.684-1.352l2.914-3.086Z"
-                    clip-rule="evenodd"
+                    clipRule="evenodd"
                 />
                 <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     d="M19.846 4.318a2.148 2.148 0 0 0-.437-.692 2.014 2.014 0 0 0-.654-.463 1.92 1.92 0 0 0-1.544 0 2.014 2.014 0 0 0-.654.463l-.546.578 2.852 3.02.546-.579a2.14 2.14 0 0 0 .437-.692 2.244 2.244 0 0 0 0-1.635ZM17.45 8.721 14.597 5.7 9.82 10.76a.54.54 0 0 0-.137.27l-.536 2.84c-.07.37.239.696.588.622l2.682-.567a.492.492 0 0 0 .255-.145l4.778-5.06Z"
-                    clip-rule="evenodd"
+                    clipRule="evenodd"
                 />
             </svg>
             EDIT
