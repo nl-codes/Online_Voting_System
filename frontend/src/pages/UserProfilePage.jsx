6e3fba381849cb83f58b71f247a8763c463be606
narayanLohani <narayanlohani07@gmail.com>
2025-05-03 13:35:00 +0545

    Call UserId for dispaly use Data

--- frontend/src/pages/UserProfilePage.jsx
+++ frontend/src/pages/UserProfilePage.jsx
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import BackButton from "../components/BackButton";
+import { UserContext } from "../context/UserContext";
 
 const UserProfilePage = () => {
+    const { userId } = useContext(UserContext);
     return (
         <div className="bg-[#29142e] h-screen max-w-screen py-4 px-4">
             <BackButton backPath={"/home"} />
             <h1 className="w-full text-4xl text-white font-bold text-center pt-10">
                 User Profile
             </h1>
+            <UserProfile userId={userId} />
         </div>
     );
 };
