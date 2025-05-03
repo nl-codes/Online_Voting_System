beea48dfe3af80244e42051d3ad426c5294908e6
narayanLohani <narayanlohani07@gmail.com>
2025-05-03 15:16:03 +0545

    Add edit button to Profile Page

--- frontend/src/pages/UserProfilePage.jsx
+++ frontend/src/pages/UserProfilePage.jsx
@@ -2,12 +2,16 @@ import React, { useContext } from "react";
 import BackButton from "../components/BackButton";
 import { UserContext } from "../context/UserContext";
 import UserProfile from "../components/UserProfile";
+import EditButton from "../components/EditButton";
 
 const UserProfilePage = () => {
     const { userId } = useContext(UserContext);
     return (
         <div className="bg-[#29142e] h-screen max-w-screen py-4 px-4">
-            <BackButton backPath={"/home"} />
+            <div className="flex justify-between items-center ">
+                <BackButton backPath={"/home"} />
+                <EditButton editPath={"/home"} />
+            </div>
             <h1 className="w-full text-4xl text-white font-bold text-center pt-10">
                 User Profile
             </h1>
