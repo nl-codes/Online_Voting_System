32c5ac1fbb792187336aadc21610f732a238edb1
Sandip <sandeepdhakal.sd@gmail.com>
2025-05-04 08:38:45 +0545

    added admin candidate card

---
+++ frontend/src/components/AdminCandidateCard.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ProfileDisplay from "./ProfileDisplay";
+
+const AdminCandidateCard = ({ id, photo_url, full_name, saying }) => {
+    return (
+        <div className="flex items-center text-white pl-8">
+            <p className="mx-8 font-bold text-xl">{id}</p>
+            <ProfileDisplay
+                image_url={photo_url}
+                sizes={{ width: 100, height: 100 }}
+            />
+            <div className="full_name-saying flex flex-col ml-8 font-serif gap-2">
+                <p className="text-3xl font-bold text-[#c791d4]">{full_name}</p>
+                <p className="text-md w-100">{saying}</p>
+            </div>
+        </div>
+    );
+};
+
+export default AdminCandidateCard;
