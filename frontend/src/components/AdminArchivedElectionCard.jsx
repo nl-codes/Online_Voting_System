4ee91d595061d6b7b7596afb63b01d490adbb733
narayanLohani <narayanlohani07@gmail.com>
2025-05-06 13:32:13 +0545

    Added Header Section

--- frontend/src/components/AdminArchivedElectionCard.jsx
+++ frontend/src/components/AdminArchivedElectionCard.jsx
@@ -23,7 +23,29 @@ const AdminArchivedElectionCard = ({ election }) => {
     const candidates = original_candidates.split("|");
     const votes = original_votes.split("|");
 
-    return <div></div>;
+    return (
+        <div className="archived-election-card flex flex-col w-full max-w-3xl bg-[#29142e] text-[#f6eef8] p-6 m-4 rounded-xl border-[#f6eef8] border-4 shadow-lg shadow-[#f6eef8] hover:shadow-xl transition-all duration-300">
+            {/* Header Section */}
+            <div className="header-section flex justify-between items-start mb-6">
+                <div>
+                    <h2 className="font-bold text-2xl mb-2">
+                        {id + ". " + original_topic}
+                    </h2>
+                    <p className="font-semibold text-lg">
+                        Position :{" "}
+                        <span className="text-gray-300 text-lg">
+                            {original_position}
+                        </span>
+                    </p>
+                </div>
+                <div className="archived-at bg-[#334155] p-3 rounded-lg">
+                    <span className="font-bold text-yellow-400">
+                        Archived At: {formatDate(archived_at)}
+                    </span>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default AdminArchivedElectionCard;
