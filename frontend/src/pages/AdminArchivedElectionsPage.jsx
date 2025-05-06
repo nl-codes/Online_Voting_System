3c2fee296f278c687c90dc02213586cb4e1f72ce
narayanLohani <narayanlohani07@gmail.com>
2025-05-06 11:03:32 +0545

    basic setup of Archive page

--- frontend/src/pages/AdminArchivedElectionsPage.jsx
+++ frontend/src/pages/AdminArchivedElectionsPage.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import AdminSidebar from "../components/AdminSidebar";
 
 const AdminArchivedElectionsPage = () => {
-    return <div>AdminArchivedElectionsPage</div>;
+    return (
+        <div className="bg-[#29142e] min-h-screen max-w-screen flex pr-8">
+            <AdminSidebar />
+            <div className="flex flex-col pt-10 pl-4 w-full text-white main-page-content">
+                <p className="text-center text-2xl font-bold">
+                    Archived Elections List
+                </p>
+                <div></div>
+            </div>
+        </div>
+    );
 };
 
 export default AdminArchivedElectionsPage;
