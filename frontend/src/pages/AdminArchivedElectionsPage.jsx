4e1521b483ba721144d005d53503bdbce22af5d9
narayanLohani <narayanlohani07@gmail.com>
2025-05-06 14:26:18 +0545

    Show the cards in center

--- frontend/src/pages/AdminArchivedElectionsPage.jsx
+++ frontend/src/pages/AdminArchivedElectionsPage.jsx
@@ -36,7 +36,7 @@ const AdminArchivedElectionsPage = () => {
     return (
         <div className="bg-[#29142e] min-h-screen max-w-screen flex pr-8">
             <AdminSidebar />
-            <div className="flex flex-col pt-10 pl-4 w-full text-white main-page-content">
+            <div className="flex flex-col items-center pt-10 pl-4 w-full text-white main-page-content">
                 <p className="text-center text-2xl font-bold">
                     Archived Elections List
                 </p>
