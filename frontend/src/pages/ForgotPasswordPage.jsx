55ae62ec1128c9576e0a1c7f00eb3b195b99dd9f
narayanLohani <narayanlohani07@gmail.com>
2025-05-09 00:56:03 +0545

    Added basic page description

--- frontend/src/pages/ForgotPasswordPage.jsx
+++ frontend/src/pages/ForgotPasswordPage.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const ForgotPasswordPage = () => {
-    return <div>ForgotPasswordPage</div>;
+    return (
+        <div className="min-h-screen w-full flex items-center justify-center bg-[#29142e] p-4">
+            <div className="rounded-3xl p-8 w-100 bg-white">
+                <h2 className="text-3xl font-bold mb-6 text-[#564181] text-center">
+                    Forgot Password
+                </h2>
+
+                <Link
+                    to="/login"
+                    className="mt-4 text-[#5D76EE] hover:underline">
+                    Back to Login
+                </Link>
+            </div>
+        </div>
+    );
 };
 
 export default ForgotPasswordPage;
