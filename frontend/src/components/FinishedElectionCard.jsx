0a9cbc61a3388a4b263d4b0c6b415e458c9827e4
narayanLohani <narayanlohani07@gmail.com>
2025-05-10 13:16:11 +0545

    Show election details

--- frontend/src/components/FinishedElectionCard.jsx
+++ frontend/src/components/FinishedElectionCard.jsx
@@ -1,6 +1,6 @@
 const FinishedElectionCard = ({ electionDetails }) => {
     const {
-        title,
+        topic,
         description,
         position,
         start_time,
@@ -17,7 +17,43 @@ const FinishedElectionCard = ({ electionDetails }) => {
     const sayings = candidate_saying_list.split("|");
     const votes = candidate_vote_list.split("|");
 
-    return <div className="text-white"></div>;
+    return (
+        <div className="election-card flex flex-col w-200 justify-center  bg-[#29142e] text-white p-4 m-4 rounded-xl border-white border-4 shadow-lg shadow-white">
+            {/* Election Details */}
+            <div>
+                {/* Election */}
+                <div>
+                    {/* Topic */}
+                    <div className="text-3xl">{topic} </div>
+                    {/* Description */}
+                    <div className="flex flex-col">
+                        <span className="text-2xl font-semibold text-[#c791d4]">
+                            Description
+                        </span>
+                        <span className="text-gray-200">{description}</span>
+                    </div>
+                    {/* Position */}
+                    <div className="flex flex-col">
+                        <span className="text-2xl font-semibold text-[#c791d4]">
+                            Position
+                        </span>
+                        <span className="text-gray-200">{position}</span>
+                    </div>
+                    {/* Timeline */}
+                    <div className="flex flex-col">
+                        <span className="text-2xl font-semibold text-[#c791d4]">
+                            Timeline
+                        </span>
+                        <span className="text-gray-200">{`${start_time} - ${stop_time}`}</span>
+                    </div>
+                </div>
+                {/* Winner */}
+                <div></div>
+            </div>
+            {/* Candidates */}
+            <div></div>
+        </div>
+    );
 };
 
 export default FinishedElectionCard;
