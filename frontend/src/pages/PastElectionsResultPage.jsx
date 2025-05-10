68c0e404804019becfae328d6027974286b0e5c2
narayanLohani <narayanlohani07@gmail.com>
2025-05-10 09:12:15 +0545

    Add dummy data to work on frontend

--- frontend/src/pages/PastElectionsResultPage.jsx
+++ frontend/src/pages/PastElectionsResultPage.jsx
@@ -1,6 +1,16 @@
 import BackButton from "../components/BackButton";
 
 const PastElectionsResultPage = () => {
+    const electionDetails = {
+        title: "This is a title",
+        description: "This is a description",
+        position: "This is a position",
+        start_time: "This is a start_time",
+        stop_time: "This is a stop_time",
+        candidate_photo_list: "",
+        candidate_name_list: "",
+        candidate_saying_list: "",
+    };
     return (
         <div className="w-screen h-screen bg-[#29142e] text-white p-8">
             {/* Header */}
