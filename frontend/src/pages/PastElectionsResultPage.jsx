f36362f838738cdde2c3ebd24d4d6241845960b9
narayanLohani <narayanlohani07@gmail.com>
2025-05-10 10:26:17 +0545

    Fetch election data from the backend api

--- frontend/src/pages/PastElectionsResultPage.jsx
+++ frontend/src/pages/PastElectionsResultPage.jsx
@@ -1,18 +1,33 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BackButton from "../components/BackButton";
+import axios from "axios";
+import { API_BASE_URL } from "../config/api";
 
 const PastElectionsResultPage = () => {
-    const [electionDetails, setElectionDetails] = useState({
-        title: "",
-        description: "",
-        position: "",
-        start_time: "",
-        stop_time: "",
-        candidate_photo_list: "",
-        candidate_name_list: "",
-        candidate_saying_list: "",
-        candidate_votes_list: "",
-    });
+    const [finishedElections, setFinishedElections] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState("");
+
+    useEffect(() => {
+        const fetchFinishedElections = async () => {
+            try {
+                const response = await axios.get(
+                    `${API_BASE_URL}/finished_election_details`
+                );
+                if (response.data.success) {
+                    setFinishedElections(response.data.data);
+                    console.log(response.data.data);
+                }
+            } catch (error) {
+                setError("Failed to fetch election results");
+                console.error("Error fetching results:", error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchFinishedElections();
+    }, []);
     return (
         <div className="w-screen h-screen bg-[#29142e] text-white p-8">
             {/* Header */}
