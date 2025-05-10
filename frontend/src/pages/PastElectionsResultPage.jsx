5ca562fb498fc25a4021cefb49a8163b8ca5a950
narayanLohani <narayanlohani07@gmail.com>
2025-05-10 08:28:37 +0545

    Add header section with page title and Back Button

--- frontend/src/pages/PastElectionsResultPage.jsx
+++ frontend/src/pages/PastElectionsResultPage.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import BackButton from "../components/BackButton";
 
 const PastElectionsResultPage = () => {
-    return <div>PastElectionsResultPage</div>;
+    return (
+        <div className="w-screen h-screen bg-[#29142e] text-white p-8">
+            {/* Header */}
+            <div className="relative flex items-center">
+                <div className="absolute left-0">
+                    <BackButton backPath={"/home"} />
+                </div>
+                <p className="w-full text-center text-4xl font-bold">
+                    Results of Past{" "}
+                </p>
+            </div>
+        </div>
+    );
 };
 
 export default PastElectionsResultPage;
